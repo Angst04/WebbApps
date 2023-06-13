7a450b291ff7e29562472bbb6afa9c7a0b3d8afb
Justifall <84130430+Angst04@users.noreply.github.com>
2023-06-14 00:20:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ let tg = window.Telegram.WebApp;
 const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 
-tg.MainButton.show()
-tg.MainButton.text = "Отправить"; 
-tg.MainButton.textColor = "#ffffff"; 
-tg.MainButton.color = "#4e4e4e"; 
-
-
 let theme = document.getElementById('theme');
 if (prefersDarkMode) {
    theme.href = 'dark-mode.css'
+   tg.MainButton.textColor = "#ffffff"; 
+   tg.MainButton.color = "#4e4e4e"; 
 } else {
    theme.href = 'light-mode.css'
+   tg.MainButton.textColor = tg.button_color; 
+   tg.MainButton.color = tg.button_text_color; 
 };
+
+
+tg.MainButton.show()
+tg.MainButton.text = "Отправить"; 
 
 
 function outputUpdate(vol) {
